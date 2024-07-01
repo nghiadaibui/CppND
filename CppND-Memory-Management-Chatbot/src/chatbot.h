9a5ef862619951d30aab6caca3afeab28ca5e3cc
nghiadaibui <nghiadaibui1999@gmail.com>
2024-07-01 21:55:35 +0700

    CppND-Memory-Management-Chatbot

---
+++ CppND-Memory-Management-Chatbot/src/chatbot.h
@@ -0,0 +1,55 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+#include <iostream>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();
+
+    //// STUDENT CODE
+    ////
+    ChatBot(const ChatBot &source);
+    ChatBot &operator=(const ChatBot &source);
+
+    ChatBot(ChatBot &&other);
+    ChatBot &operator=(ChatBot &&other);
+    ////
+    //// EOF STUDENT CODE
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    // ChatLogic* GetChatLogicHandle() { return _chatLogic; }
+    wxBitmap *GetImageHandle() { 
+      std::cout << "GetImageHandle " << _image << std::endl;
+      return _image;
+    }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
